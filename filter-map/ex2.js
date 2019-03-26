861f896380068650656cba23fdd1201820072feb
Barenziah <presciente.snotrq@gmail.com>
2019-03-26 11:11:31 +0100

    Map + filter is SOOOO much easier than filter 6 T.T

--- filter-map/ex2.js
+++ filter-map/ex2.js
@@ -23,8 +23,19 @@ Sortie attendue:
 ['Bordeaux', 'Lille', 'Marseille', 'Reims', 'Toulouse']
 
 */
+let testArr = [
+  { city: 'Bordeaux', curriculums: ['PHP/Symfony', 'JS/React'] },
+  { city: 'La Loupe', curriculums: ['JS/Angular'] },
+  { city: 'Lille', curriculums: ['PHP/Symfony', 'JS/React'] },
+  { city: 'Marseille', curriculums: ['JS/React'] },
+  { city: 'Orléans', curriculums: ['PHP/Symfony'] },
+  { city: 'Reims', curriculums: ['JS/React'] },
+  { city: 'Toulouse', curriculums: ['JEE/Android', 'JS/React'] }
+]
 
-function getCampusesTeachingReact(campuses) {
-}
+
+const getCampusesTeachingReact = (array) => array.filter((elem) => {
+  return elem.curriculums.includes('JS/React');
+}).map(elem => elem.city);
 
 module.exports = getCampusesTeachingReact;
